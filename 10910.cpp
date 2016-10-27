a765f2d361770926748f581f0228501f23d0cf8c
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2016-10-27 19:32:02 -0200

    Update 10910.cpp

--- 10910.cpp
+++ 10910.cpp
@@ -12,19 +12,19 @@ int num_subjects, total_marks, min_marks;
 
 map<pair<int, int>, int> num_ways;
 
-int calc(int total, int depth = 1) {
+int calc(int remain, int depth = 1) {
     map<pair<int, int>, int>::iterator it;
-    if ((it = num_ways.find({total, depth})) != num_ways.end())
+    if ((it = num_ways.find({remain, depth})) != num_ways.end())
         return it->second;
 
     if (depth >= num_subjects)
-        return num_ways[{total, depth}] = 1;
+        return num_ways[{remain, depth}] = 1;
 
     int ways = 0;
-    for (int i = 0; i <= total; ++i)
+    for (int i = 0; i <= remain; ++i)
         ways += calc(i, depth + 1);
 
-    return num_ways[{total, depth}] = ways;
+    return num_ways[{remain, depth}] = ways;
 }
 
 int main() {
