86bea9c87125466e08ecdeff6d16be95b8093ae4
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2016-10-27 19:25:20 -0200

    Update 10910.cpp

--- 10910.cpp
+++ 10910.cpp
@@ -12,29 +12,29 @@ int num_subjects, total_marks, min_marks;
 
 map<pair<int, int>, int> num_ways;
 
-int calc(int resto, int nivel = 1) {
-	map<pair<int, int>, int>::iterator it;
-	if ((it = num_ways.find({resto, nivel})) != num_ways.end())
-		return it->second;
+int calc(int total, int depth = 1) {
+    map<pair<int, int>, int>::iterator it;
+    if ((it = num_ways.find({total, depth})) != num_ways.end())
+        return it->second;
 
-	if (nivel >= num_subjects)
-		return num_ways[{resto, nivel}] = 1;
+    if (depth >= num_subjects)
+        return num_ways[{total, depth}] = 1;
 
-	int ways = 0;
-	for (int i = 0; i <= resto; ++i)
-		ways += calc(i, nivel + 1);
+    int ways = 0;
+    for (int i = 0; i <= total; ++i)
+        ways += calc(i, depth + 1);
 
-	return num_ways[{resto, nivel}] = ways;
+    return num_ways[{total, depth}] = ways;
 }
 
 int main() {
-	cin >> num_tests;
-	
-	while (num_tests--) {
-		cin >> num_subjects >> total_marks >> min_marks;
-		cout << calc(total_marks - num_subjects * min_marks) << '\n';
-		num_ways.clear();
-	}
+    cin >> num_tests;
+    
+    while (num_tests--) {
+        cin >> num_subjects >> total_marks >> min_marks;
+        cout << calc(total_marks - num_subjects * min_marks) << '\n';
+        num_ways.clear();
+    }
 
-	return 0;
+    return 0;
 }
